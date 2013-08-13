682fe257d0d6fd1e3e3f55d41495a2d9eac4b700
Elias Rabel <e.rabel@fz-juelich.de>
2013-08-13 10:27:28 +0200

    Corrected script to get cartesian forces.

--- source/KKRnano/kkr2/getforces.py
+++ source/KKRnano/kkr2/getforces.py
@@ -5,15 +5,32 @@
 import struct
 
 DOUBLESIZE = 8
-f = open('forces', 'rb')
-fout = open('forces.dat', 'w')
 
-while True:
-    x = f.read(DOUBLESIZE*3)
-    if x == '':
-        break
-    force = struct.unpack("3d", x)
-    fout.write("{} {} {}\n".format(*force))
+def forces_reader(f):
+    """Reader to extract forces from binary file f."""
+    while True:
+        x = f.read(DOUBLESIZE*3)
+        if x == '':
+            break
+        force = struct.unpack("3d", x)
+        # Force components are not in correct order for cartesian output
+        # See definition of real spherical harmonics with l=1
+        yield force[2], force[0], force[1]
 
-f.close()
-fout.close()
+
+def main():
+    """Main routine: Read forces from binary file and write to text file."""
+
+    f = open('forces', 'rb')
+    fout = open('forces.dat', 'w')
+    
+    force_file = forces_reader(f)
+    
+    for force in force_file:
+        fout.write("{} {} {}\n".format(*force))
+    
+    f.close()
+    fout.close()
+
+if __name__ == "__main__":
+    main()
