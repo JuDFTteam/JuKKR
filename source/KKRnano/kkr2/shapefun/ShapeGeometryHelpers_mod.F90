bf50d54fe4cf08415c4b807bafa5559331b0d97c
Paul F. Baumeister <p.baumeister@fz-juelich.de>
2015-08-26 11:16:34 +0200

    make less use of common blocks for Euler angles

--- source/KKRnano/kkr2/shapefun/ShapeGeometryHelpers_mod.F90
+++ source/KKRnano/kkr2/shapefun/ShapeGeometryHelpers_mod.F90
@@ -4,7 +4,16 @@ module ShapeGeometryHelpers_mod
   implicit none
   private
 
-  public :: polchk, perp
+  public :: polchk, perp, nrm2, operator(.dot.)
+  
+  interface operator(.dot.)
+    module procedure inner_product_v3
+  endinterface
+  
+  interface nrm2
+    module procedure norm2_squared_v3
+  endinterface
+  
 
   contains
 !---------------------------------------------------------------------
@@ -27,13 +36,12 @@ module ShapeGeometryHelpers_mod
 
     integer, intent(in) :: nface
     integer, intent(in) :: nvertices(:)
-    double precision, intent(in) :: xvert(:,:),yvert(:,:),zvert(:,:)
+    double precision, intent(in) :: xvert(:,:),yvert(:,:),zvert(:,:) ! todo: unite into an array of shape vert(3,nvertd,nfaced)
     double precision, intent(in) :: tolvdist
 
-    integer :: ivert,inew,ivertp,ivertm,ivrt,iedge,nvrt,nedge
-    integer :: iface,nvert
+    integer :: ivert,inew,ivertp,ivertm,ivrt,iedge,nvrt,nedge, iface,nvert
     double precision :: arg,a1,a2,down,up,fisum,t
-    double precision :: vrtx,vrty,vrtz,vrtpx,vrtpy,vrtpz,vrtmx,vrtmy,vrtmz
+    double precision :: vrt0(3), vrtp(3), vrtm(3)
     double precision :: pi314
     double precision, allocatable :: v1(:,:),v2(:,:),v(:,:),vrt(:,:) ! v1(3,nedged),v2(3,nedged),v(3,nvertd),vrt(3,nvrtd)
     integer :: nfaced, nedged, nvertd, nvrtd
@@ -43,56 +51,52 @@ module ShapeGeometryHelpers_mod
     nvrtd   =  nfaced*nvertd
     nedged  =  nvrtd+nfaced-2
 
-    allocate(v1(3,nedged),v2(3,nedged),v(3,nvertd),vrt(3,nvrtd))
+    allocate(v1(3,nedged), v2(3,nedged), v(3,nvertd), vrt(3,nvrtd))
 
-    pi314  =  4.d0*atan(1.d0)
+    pi314 = 4.d0*atan(1.d0)
 
     nvrt = 0
     nedge = 0
-    do iface = 1,nface
+    do iface = 1, nface
       nvert = nvertices(iface)
       fisum = (nvert-2)*pi314
       do ivert = 1, nvert
-        v(1,ivert) = xvert(ivert,iface)
-        v(2,ivert) = yvert(ivert,iface)
-        v(3,ivert) = zvert(ivert,iface)
+        v(1:3,ivert) = [xvert(ivert,iface), yvert(ivert,iface), zvert(ivert,iface)]
       enddo ! ivert
 !
 !------> t r e a t m e n t   o f   v e r t i c e s
 !
       do ivert = 1, nvert
-        vrtx = v(1,ivert)
-        vrty = v(2,ivert)
-        vrtz = v(3,ivert)
+        vrt0 = v(1:3,ivert)
         inew = 1 ! 1:save all different vertices
-        do ivrt = 1,nvrt
-          t = (vrtx-vrt(1,ivrt))**2 + (vrty-vrt(2,ivrt))**2 + (vrtz-vrt(3,ivrt))**2
-          if(t < tolvdist) inew = 0
+        do ivrt = 1, nvrt
+!         t = (vrt0x-vrt(1,ivrt))**2 + (vrt0y-vrt(2,ivrt))**2 + (vrt0z-vrt(3,ivrt))**2
+          if (nrm2(vrt0 - vrt(1:3,ivrt)) < tolvdist) inew = 0 ! 0:drop this vertex
         enddo ! ivrt
         
-        if (inew  ==  1) then
+        if (inew == 1) then
           nvrt = nvrt+1
           if(nvrt > nvrtd) stop 'increase nvrtd'
-          vrt(1,nvrt) = v(1,ivert)
-          vrt(2,nvrt) = v(2,ivert)
-          vrt(3,nvrt) = v(3,ivert)
-        endif
-        ivertp = ivert+1
-        if (ivert == nvert) ivertp = 1
-        vrtpx = v(1,ivertp)
-        vrtpy = v(2,ivertp)
-        vrtpz = v(3,ivertp)
-        ivertm = ivert-1
-        if (ivert == 1) ivertm = nvert
-        vrtmx = v(1,ivertm)
-        vrtmy = v(2,ivertm)               ! check if the  consecutive
-        vrtmz = v(3,ivertm)               ! vertices define a polygon
-        a1 = sqrt((vrtpx-vrtx)**2 + (vrtpy-vrty)**2 + (vrtpz-vrtz)**2)
-        a2 = sqrt((vrtmx-vrtx)**2 + (vrtmy-vrty)**2 + (vrtmz-vrtz)**2)
-        down = a1*a2
-        up = (vrtpx-vrtx)*(vrtmx-vrtx) + (vrtpy-vrty)*(vrtmy-vrty) + (vrtpz-vrtz)*(vrtmz-vrtz)
+          vrt(1:3,nvrt) = v(1:3,ivert)
+        endif ! inew
+        
+        ivertp = ivert+1; if (ivert == nvert) ivertp = 1        !!! alternative: ivertp = modulo(ivert+1-1, nvert)+1
+        vrtp = v(1:3,ivertp)
+        ivertm = ivert-1; if (ivert == 1) ivertm = nvert        !!! alternative: ivertm = modulo(ivert-1-1, nvert)+1
+        vrtm = v(1:3,ivertm) ! check if the  consecutive vertices define a polygon
+        
+!         a1 = sqrt((vrtpx-vrt0x)**2 + (vrtpy-vrt0y)**2 + (vrtpz-vrt0z)**2)
+!         a2 = sqrt((vrtmx-vrt0x)**2 + (vrtmy-vrt0y)**2 + (vrtmz-vrt0z)**2)
+!         up = (vrtpx-vrt0x)*(vrtmx-vrt0x) + (vrtpy-vrt0y)*(vrtmy-vrt0y) + (vrtpz-vrt0z)*(vrtmz-vrt0z)
+!         down = a1*a2
+
+        a1 = nrm2(vrtp - vrt0)
+        a2 = nrm2(vrtm - vrt0)
+        up = (vrtp - vrt0) .dot. (vrtm - vrt0)
+        down = sqrt(a1*a2)
+        
         ! write(6,*)
-        ! write(6,*) vrtx,vrty,vrtz
+        ! write(6,*) vrt0x,vrt0y,vrt0z
         ! write(6,*) vrtmx,vrtmy,vrtmz
         ! write(6,*) vrtpx,vrtpy,vrtpz
         ! write(6,*) 'fisum ',ivert,a1,a2,up,arg,acos(arg)
@@ -109,33 +113,28 @@ module ShapeGeometryHelpers_mod
 !
         inew = 1 ! 1:save all different edges
         do iedge = 1, nedge
-          t = (vrtx-v1(1,iedge))**2 + (vrty-v1(2,iedge))**2 + (vrtz-v1(3,iedge))**2
-          if (t < tolvdist) then
-            t = (vrtpx-v2(1,iedge))**2 + (vrtpy-v2(2,iedge))**2 + (vrtpz-v2(3,iedge))**2
-            if(t < tolvdist) inew = 0
+          if (nrm2(vrt0 - v1(1:3,iedge)) < tolvdist) then
+            if (nrm2(vrtp - v2(1:3,iedge)) < tolvdist) inew = 0 ! 0:do not save
           else
-            t = (vrtx-v2(1,iedge))**2 + (vrty-v2(2,iedge))**2 + (vrtz-v2(3,iedge))**2
-            if(t < tolvdist) then
-              t = (vrtpx-v1(1,iedge))**2 + (vrtpy-v1(2,iedge))**2 + (vrtpz-v1(3,iedge))**2
-              if(t < tolvdist) inew = 0
+            if (nrm2(vrt0 - v2(1:3,iedge)) < tolvdist) then
+              if (nrm2(vrtp - v1(1:3,iedge)) < tolvdist) inew = 0 ! 0:do not save
             endif
           endif
         enddo ! iedge
+        
         if (inew == 1) then
           nedge = nedge+1
-          if(nedge > nedged) stop 'insufficient nedged'
-          v1(1,nedge) = v(1,ivert )
-          v1(2,nedge) = v(2,ivert )
-          v1(3,nedge) = v(3,ivert )
-          v2(1,nedge) = v(1,ivertp)
-          v2(2,nedge) = v(2,ivertp)
-          v2(3,nedge) = v(3,ivertp)
+          if (nedge > nedged) stop 'insufficient nedged'
+          v1(1:3,nedge) = v(1:3,ivert)
+          v2(1:3,nedge) = v(1:3,ivertp)
         endif ! inew
+        
       enddo ! ivert
-      if(fisum > 1d-6) then
+      if (fisum > 1d-6) then
         write(6,*) 'fisum  = ',fisum
         stop 'not consecutive vertices of a polygon'
-      end if
+      endif ! fisum
+      
     enddo ! iface
     if ((nvrt+nface) /= (nedge+2)) then
       write(6,*) '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
@@ -143,6 +142,7 @@ module ShapeGeometryHelpers_mod
       write(6,*) '   >>  stop illegal polyhedron      '
       write(6,*) 'nvrt = ',nvrt,' ; nface = ',nface,' ; nedge = ',nedge
     endif
+    
   endsubroutine polchk
 
 !-----------------------------------------------------------------------
@@ -158,28 +158,40 @@ module ShapeGeometryHelpers_mod
     double precision, intent(in) :: r0(3), r1(3), r2(3)
     double precision, intent(in) :: tolvdist
 !---------------------------------------------------------------------
-    double precision :: dx,dy,dz,s,d,da,db,dc,d1,d2,co
+!     double precision :: dx,dy,dz,s,d,da,db,dc,d1,d2,co
+    double precision :: dxyz(3), dabc(3), s, d
     
-    dx = r2(1) - r1(1)
-    dy = r2(2) - r1(2)
-    dz = r2(3) - r1(3)
-    s = r0(1)*dx + r0(2)*dy + r0(3)*dz
-    d = dx*dx + dy*dy + dz*dz
-    if(d < tolvdist*tolvdist) then
+!     dx = r2(1) - r1(1)
+!     dy = r2(2) - r1(2)
+!     dz = r2(3) - r1(3)
+    dxyz = r2 - r1    
+!   s = r0(1)*dx + r0(2)*dy + r0(3)*dz
+    s = r0 .dot. dxyz
+!   d = dx*dx + dy*dy + dz*dz
+    d = nrm2(dxyz)
+    
+    if (d < tolvdist*tolvdist) then
       write(6,fmt="(///33x,'from perp:   identical points'/33x,2('(',3e14.6,')',3x))") r1(1:3), r2(1:3)
       stop
-    endif      
-    da = s*dx + dy*(r1(1)*r2(2) - r1(2)*r2(1)) + dz*(r1(1)*r2(3) - r1(3)*r2(1))
-    db = s*dy + dz*(r1(2)*r2(3) - r1(3)*r2(2)) + dx*(r1(2)*r2(1) - r1(1)*r2(2))
-    dc = s*dz + dx*(r1(3)*r2(1) - r1(1)*r2(3)) + dy*(r1(3)*r2(2) - r1(2)*r2(3))
-    rd(1) = da/d
-    rd(2) = db/d
-    rd(3) = dc/d
-    d1 = (rd(1) - r1(1))**2 + (rd(2) - r1(2))**2 + (rd(3) - r1(3))**2
-    d2 = (rd(1) - r2(1))**2 + (rd(2) - r2(2))**2 + (rd(3) - r2(3))**2
+    endif
+    
+    dabc(1) = s*dxyz(1) + dxyz(2)*(r1(1)*r2(2) - r1(2)*r2(1)) + dxyz(3)*(r1(1)*r2(3) - r1(3)*r2(1))
+    dabc(2) = s*dxyz(2) + dxyz(3)*(r1(2)*r2(3) - r1(3)*r2(2)) + dxyz(1)*(r1(2)*r2(1) - r1(1)*r2(2))
+    dabc(3) = s*dxyz(3) + dxyz(1)*(r1(3)*r2(1) - r1(1)*r2(3)) + dxyz(2)*(r1(3)*r2(2) - r1(2)*r2(3))
+    rd(1:3) = dabc/d
 
-    co = d - max(d1, d2)
-    inside = (co > tolvdist)
+    inside = (d - max(nrm2(rd - r1), nrm2(rd - r2)) > tolvdist)
   endsubroutine perp
+  
+  double precision function inner_product_v3(v, w) result(vxw)
+    double precision, intent(in) :: v(3), w(3)
+    vxw = v(1)*w(1) + v(2)*w(2) + v(3)*w(3)
+  endfunction
+  
+  double precision function norm2_squared_v3(v) result(vxv)
+    double precision, intent(in) :: v(3)
+    vxv = v(1)*v(1) + v(2)*v(2) + v(3)*v(3)
+  endfunction
+  
 
 endmodule ShapeGeometryHelpers_mod
