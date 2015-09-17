cbef4f0d473ddfa5cdfc8d5387658587736394ee
Paul F. Baumeister <p.baumeister@fz-juelich.de>
2015-09-17 18:16:00 +0200

    remove externally linked erfcex.f

--- source/KKRnano/kkr2/madelung/MadelungCalculator_mod.F90
+++ source/KKRnano/kkr2/madelung/MadelungCalculator_mod.F90
@@ -885,7 +885,6 @@ subroutine strmat(alat, lmax, naez, ngmax, nrmax, nlshellg, nlshellr, gv, rv, qv
     integer, intent(in) :: lmax
     double precision, intent(in) :: alpha, r
 
-    double precision, external :: erfcex
     double precision :: arg, facl, fex
     integer :: l
 
@@ -908,7 +907,67 @@ subroutine strmat(alat, lmax, naez, ngmax, nrmax, nlshellg, nlshellr, gv, rv, qv
       glh(l) = glh(l)*fex
     enddo ! l
 
-  endfunction gamfc
+  endfunction ! gamfc
   
+  
+  
+  double precision function erfcex(z)
+    double precision, intent(in) :: z
+    ! calculates complementary errorfunction times sqrt(pi) times exp(z*z)  by continued fractions
+    double precision, parameter :: bound = 3.d-11
+    double precision :: erf1, exzz, f, fa, q, denom, sqrtpi, term, u, ua, v, x, xa, y, tz2, z2
+    logical :: run
+    
+    sqrtpi = sqrt(4.d0*atan(1.d0))
+    z2 = z*z
+    
+    !---> choose algorithm
+    if (z < 1.5d0) then
+      ! this exponential was outside the if statement but for large arguments the exponent blow up
+
+      exzz = exp(z2)
+
+      tz2 = 2.d0*z2
+      erf1 = z
+      denom = 1.d0
+      term = z
+      
+      run = .true. ! always do at least one iteration
+      do while (run)
+        denom = denom + 2.d0
+        term = term*tz2/denom
+        erf1 = erf1 + term
+        run = (term > bound)
+      enddo ! while
+      
+      erfcex = sqrtpi*exzz - 2.d0*erf1
+
+    else
+      ! continued fraction expansion : abramowitz p. 298, eq. (7.1.14)
+      u = 1.d0
+      v = 0.d0
+      x = z
+      y = 1.d0
+      q = 0.5d0
+      f = (u + v*q)/(x + y*q)
+      
+      run = .true. ! always do at least one iteration
+      do while (run)
+        ua = u
+        u = u*z + v*q
+        v = ua
+        xa = x
+        x = x*z + y*q
+        y = xa
+        q = q + 0.5d0
+        fa = f
+        f = (u + v*q)/(x + y*q)
+        run = (abs(fa - f) > bound*f)
+      enddo ! while
+      
+      erfcex = f
+    endif
+
+  endfunction ! erfcex
   
 endmodule MadelungCalculator_mod
--- source/KKRnano/kkr2/madelung/erfcex.f90
+++ source/KKRnano/kkr2/madelung/erfcex.f90
@@ -6,11 +6,12 @@
     !      times exp(z*z)  by continued fractions
     !-----------------------------------------------------------------------
     double precision, parameter :: bound = 3.d-11
-    double precision :: erf1, exzz, f, fa, q, ratio, sqrtpi, term, u, ua, v, x, xa, y, tz2, z2
-
+    double precision :: erf1, exzz, f, fa, q, denom, sqrtpi, term, u, ua, v, x, xa, y, tz2, z2
+    logical :: run
+    
     sqrtpi = sqrt(4.d0*atan(1.d0))
     z2 = z*z
-
+    
     !---> choose algorithm
     if (z < 1.5d0) then
       ! this exponential was outside the if statement but for large arguments the exponent blow up
@@ -19,14 +20,16 @@
 
       tz2 = 2.d0*z2
       erf1 = z
-      ratio = 1.d0
+      denom = 1.d0
       term = z
       
-   10 continue
-      ratio = ratio + 2.d0
-      term = term*tz2/ratio
-      erf1 = erf1 + term
-      if (term > bound) go to 10
+      run = .true. ! always do at least one iteration
+      do while (run)
+        denom = denom + 2.d0
+        term = term*tz2/denom
+        erf1 = erf1 + term
+        run = (term > bound)
+      enddo ! while
       
       erfcex = sqrtpi*exzz - 2.d0*erf1
 
@@ -39,17 +42,19 @@
       q = 0.5d0
       f = (u + v*q)/(x + y*q)
       
-   20 continue
-      ua = u
-      u = u*z + v*q
-      v = ua
-      xa = x
-      x = x*z + y*q
-      y = xa
-      q = q + 0.5d0
-      fa = f
-      f = (u + v*q)/(x + y*q)
-      if (abs(fa - f) > bound*f) go to 20
+      run = .true. ! always do at least one iteration
+      do while (run)
+        ua = u
+        u = u*z + v*q
+        v = ua
+        xa = x
+        x = x*z + y*q
+        y = xa
+        q = q + 0.5d0
+        fa = f
+        f = (u + v*q)/(x + y*q)
+        run = (abs(fa - f) > bound*f)
+      enddo ! while
       
       erfcex = f
     endif
