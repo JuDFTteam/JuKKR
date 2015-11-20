5b63a4c9e87a40b75dd90282d9676a4043fa0586
ruess <p.ruessmann@fz-juelich.de>
2015-11-20 15:34:10 +0100

    Updated version info
    - get version script now also includes build information (serial,
    openmp, or mpi)
    - .mod files moved to temporary folder $(OBJ)

--- source/KKRhost/ALL_SOURCE_FILES/getversion.sh
+++ source/KKRhost/ALL_SOURCE_FILES/getversion.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 echo "      character(len=*), parameter :: version='" > tmpver
 git describe >> tmpver
+echo ":" >> tmpver
+cat compver >> tmpver
 echo "'" >> tmpver
 tr -d '\n' < tmpver > ALL_SOURCE_FILES/version
 echo "" >> ALL_SOURCE_FILES/version
 echo "Version: "
 git describe
-rm -f tmpver
+rm -f tmpver compver
 
