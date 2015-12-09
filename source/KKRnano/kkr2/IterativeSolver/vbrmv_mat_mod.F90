120f638d67d125df6557e1a148e4e8546f04f9bd
Paul F. Baumeister <p.baumeister@fz-juelich.de>
2015-12-09 15:45:56 +0100

    nicer matrix-application

--- source/KKRnano/kkr2/IterativeSolver/vbrmv_mat_mod.F90
+++ source/KKRnano/kkr2/IterativeSolver/vbrmv_mat_mod.F90
@@ -10,33 +10,34 @@
 
 
 module vbrmv_mat_mod
-  use TruncationZone_mod, only: clear_non_existing_entries
-#define CLEAR_NON_EXISTING_ENTRIES(B, N) call clear_non_existing_entries(B, N)
+!   use TruncationZone_mod, only: clear_non_existing_entries
+! #define CLEAR_NON_EXISTING_ENTRIES(Ax, N) call clear_non_existing_entries(Ax, N)
+#define CLEAR_NON_EXISTING_ENTRIES(Ax, N)
   implicit none
   private
   public :: multiply_vbr
   
   contains
 
-  subroutine multiply_vbr(a, x, b, sparse)
+  subroutine multiply_vbr(a, x, Ax, sparse)
     use SparseMatrixDescription_mod, only: SparseMatrixDescription
     double complex, intent(in)  :: a(:), x(:,:)
-    double complex, intent(out) :: b(:,:)
+    double complex, intent(out) :: Ax(:,:)
     type(SparseMatrixDescription), intent(in) :: sparse
 
     call vbrmv_mat(sparse%blk_nrows, sparse%ia, sparse%ja, sparse%ka, &
-                   a, sparse%kvstr, sparse%kvstr, x, b, &
+                   a, sparse%kvstr, sparse%kvstr, x, Ax, &
                    sparse%max_blockdim, sparse%max_blocks_per_row)
 
   endsubroutine ! multiply_vbr
 
   !> Heavily modified routine from SPARSKIT
-  subroutine vbrmv_mat(blk_nrows, ia, ja, ka, A, kvstr, kvstc, x, b, max_blockdim, max_blocks_per_row)
+  subroutine vbrmv_mat(blk_nrows, ia, ja, ka, A, kvstr, kvstc, x, Ax, max_blockdim, max_blocks_per_row)
                        
-    integer, intent(in) :: blk_nrows, ia(blk_nrows+1), ja(:), ka(:), kvstr(blk_nrows+1), kvstc(:)
+    integer, intent(in) :: blk_nrows, ia(blk_nrows+1), ja(:), ka(:), kvstr(:), kvstc(:)
     integer, intent(in) :: max_blockdim, max_blocks_per_row
     double complex, intent(in)  :: A(:), x(:,:)
-    double complex, intent(out) :: b(:,:)
+    double complex, intent(out) :: Ax(:,:)
     !-----------------------------------------------------------------------
     !     Sparse matrix-full vector product, in VBR format.
     !-----------------------------------------------------------------------
@@ -46,11 +47,11 @@ module vbrmv_mat_mod
     !     ia,ja,ka,A,kvstr,kvstc = matrix A in variable block row format
     !     x       = multiplier vector in full format
 
-    !     ncols = number of columns of matrix A
+    !     nRHSs = number of columns of matrix A
 
     !     On return:
     !---------------
-    !     b = product of matrix A times vector x in full format
+    !     Ax = product of matrix A times vector x in full format
 
     !     Algorithm:
     !---------------
@@ -59,51 +60,49 @@ module vbrmv_mat_mod
     !-----------------------------------------------------------------------
     !-----local variables
 
-!IBM* ALIGN(32, buffer)
-    double complex :: buffer(max_blockdim*max_blocks_per_row,size(x, 2))
+!IBM* ALIGN(32, Buffer)
+    double complex :: Buffer(max_blockdim*max_blocks_per_row,size(x, 2))
 
-    integer :: ibr, ibc, j, k, istart, ncols, icols, nrowbuf, sum_nrowbuf
-    integer :: startrow, leaddim_b, nrows, leaddim_buffer
+    integer :: ibr, isr, j, k, nRHSs, iRHSs, nblk, nsum, nrows
+    integer :: isc, ibc, leaddim_Ax, leaddim_Buffer
 
-    double complex, parameter :: CZERO = (0.d0, 0.d0), CONE  = (1.d0, 0.d0)
+    double complex, parameter :: ZERO=(0.d0, 0.d0), ONE=(1.d0, 0.d0)
 
-    leaddim_b = size(b, 1)
-    ncols = size(b, 2)
-    leaddim_buffer = max_blockdim*max_blocks_per_row
+    nRHSs      = size(Ax, 2)
+    leaddim_Ax = size(Ax, 1)
+    leaddim_Buffer = max_blockdim*max_blocks_per_row
 
-    b = CZERO
+    Ax = ZERO
 
-!     can parallelise this loop
-
-!$OMP PARALLEL PRIVATE(ibr,istart,nrows,sum_nrowbuf,j,startrow,nrowbuf,icols,buffer,k)
+!$OMP PARALLEL PRIVATE(ibr,ibc,isr,isc,nrows,nsum,nblk,j,iRHSs,Buffer,k)
 !$OMP DO
     do ibr = 1, blk_nrows
-      istart = kvstr(ibr)
-      nrows  = kvstr(ibr+1) - istart
-      sum_nrowbuf = 0
+      isr   = kvstr(ibr)
+      nrows = kvstr(ibr+1) - isr
+      nsum  = 0
 
       k = ka(ia(ibr))
       do j = ia(ibr), ia(ibr+1)-1
-        ibc = ja(j)
-        startrow = kvstc(ibc)
-        nrowbuf  = kvstc(ibc+1) - startrow
+        ibc  = ja(j)
+        isc  = kvstc(ibc)
+        nblk = kvstc(ibc+1) - isc
 
 !IBM* ASSERT(ITERCNT(16))
-        do icols = 1, ncols
-          !call ZCOPY(nrowbuf, x(startrow, icols), 1, buffer(sum_nrowbuf+1,icols), 1)
-          buffer(sum_nrowbuf+1:sum_nrowbuf+nrowbuf,icols) = x(startrow:startrow+nrowbuf-1,icols)
-        enddo ! icols
+        do iRHSs = 1, nRHSs
+          ! call ZCOPY(nblk, x(isc,iRHSs), 1, Buffer(nsum+1,iRHSs), 1)
+          Buffer(nsum+1:nsum+nblk,iRHSs) = x(isc:isc+nblk-1,iRHSs)
+        enddo ! iRHSs
 
-        sum_nrowbuf = sum_nrowbuf + nrowbuf
+        nsum = nsum + nblk
       enddo ! j
 
-      call ZGEMM('N','N',nrows,ncols,sum_nrowbuf,CONE,A(k:),nrows,buffer,leaddim_buffer,CZERO,b(istart,1),leaddim_b)
+      call ZGEMM('N', 'N', nrows, nRHSs, nsum, ONE, A(k:), nrows, Buffer, leaddim_Buffer, ZERO, Ax(isr,1), leaddim_Ax)
 
     enddo ! ibr
 !$OMP endDO
 !$OMP endPARALLEL
 
-    CLEAR_NON_EXISTING_ENTRIES(b, max_blockdim)
+    CLEAR_NON_EXISTING_ENTRIES(Ax, max_blockdim)
     
   endsubroutine ! vbrmv_mat
 
