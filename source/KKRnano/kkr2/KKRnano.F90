29d94b48a6093dee709192e398f8c0dfe0a2278d
Marcel Bornemann <m.bornemann@fz-juelich.de>
2016-07-01 16:43:01 +0200

    - no warning about LLY anymore

--- source/KKRnano/kkr2/KKRnano.F90
+++ source/KKRnano/kkr2/KKRnano.F90
@@ -153,7 +153,7 @@ program KKRnano
 
   if (params%JIJ .and. dims%nspind /= 2) die_here("Jij calculation not possible for spin-unpolarized calc.")
 
-  if (dims%LLY /= 0) die_here("Lloyds formula not supported in this version. Set lly=0")
+!  if (dims%LLY /= 0) die_here("Lloyds formula not supported in this version. Set lly=0")
 
   !=====================================================================
   ! processors not fitting in NAEZ*LMPID*SMPID*EMPID do nothing ... and wait after SC-ITER loop
