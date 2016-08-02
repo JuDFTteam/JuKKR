81d349c219fcf356c8db865924708579e70ad8c3
Paul F. Baumeister <p.baumeister@fz-juelich.de>
2016-08-02 14:20:33 +0200

    turn on COMPUTE_tref_LOCALLY

--- source/KKRnano/kkr2/ScatteringCalculation_mod.F90
+++ source/KKRnano/kkr2/ScatteringCalculation_mod.F90
@@ -172,7 +172,7 @@ implicit none
 
         ! if we had rMTref given for all atoms inside the reference cluster radius
         !   we could compute the Tref on the fly
-! #define COMPUTE_tref_LOCALLY
+#define COMPUTE_tref_LOCALLY
 #ifndef COMPUTE_tref_LOCALLY       
          Tref_local = ZERO
         dTref_local = ZERO
