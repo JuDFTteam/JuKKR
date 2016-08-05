9a6d6268c06049d47ce41a7154fd80bfa4b8654e
Bernd Zimmermann <be.zimmermann@fz-juelich.de>
2016-08-05 17:09:29 +0200

    Added MIT License: new LICENSE file and respective header in each source file.

--- test.F90
+++ test.F90
@@ -1,3 +1,10 @@
+!-----------------------------------------------------------------------------------------!
+! Copyright (c) 2016 Peter Grünberg Institut, Forschungszentrum Jülich, Germany           !
+! This file is part of kk-prime@juKKR and available as free software under the conditions !
+! of the MIT license as expressed in the LICENSE file in more detail.                     !
+!-----------------------------------------------------------------------------------------!
+
+
 program TEST
 
   use type_inc,       only: inc_type
