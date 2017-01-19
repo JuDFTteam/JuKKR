55db2110c385f82b718e015435514e8b35cb434d
Marcel Bornemann <m.bornemann@fz-juelich.de>
2017-01-19 11:42:29 +0100

    - RadialMesh is created in the correct way in Startb1_mod.F90 now

--- source/KKRnano/source/KKRzero_mod.F90
+++ source/KKRnano/source/KKRzero_mod.F90
@@ -122,11 +122,17 @@ module KKRzero_mod
     ist = getValues("input.conf", params)
     if (ist /= 0) die_here('failed to read "input.conf"!')
 
+    ! global.conf <-> input.conf consistency checks
     if (dims%KPOIBZ < params%bzdivide(1)*params%bzdivide(2)*params%bzdivide(3)) then
       dims%KPOIBZ = params%bzdivide(1)*params%bzdivide(2)*params%bzdivide(3)
       warn(6,'Kpoint allocation insufficient. KPOIBZ is increased to ' + params%bzdivide(1)*params%bzdivide(2)*params%bzdivide(3)) 
     endif
 
+    if (dims%ITDBRYD < params%scfsteps) then
+      dims%ITDBRYD = params%scfsteps
+      warn(6,'ITBRYD increased to ' + dims%ITDBRYD) 
+    endif
+    
     dims%iemxd = getEnergyMeshSize(params%npol, [params%npnt1, params%npnt2, params%npnt3], params%npntsemi)
     call create(emesh, dims%iemxd)
 
