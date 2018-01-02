f4c7d1f38074546b482b61a2756d545225d340e2
Philipp Rüssmann <p.ruessmann@fz-juelich.de>
2018-01-02 13:46:08 +0100

    Using pytest for verification of results

---
+++ source/KKRhost/tests/verify_results.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import pytest
+
+def test_verify_results():
+    import os
+    os.system('../tools/verify_test.py . printall | tee output_verifier.txt')
+    from kkrparser_functions import *
+    from pprint import pprint
+   
+    path0 = 'test_run/'
+    print_all = False
+   
+    success, parser_msgs, out_dict = parse_kkr_outputfile({}, path0+'out_kkr', path0+'output.0.txt', path0+'output.000.txt', path0+'out_timing.000.txt', path0+'out_potential', path0+'nonco_angle_out.dat')
+   
+    if not success or print_all:
+        if not success:
+            print 'WARNING parsing retured with error(s)'
+            pprint(parser_msgs)
+            print 
+        print 'parsed out_dict:'
+        pprint(out_dict)
+   
+    assert success
+    assert out_dict['convergence_group']['rms'] < 10**-6
+    assert abs(out_dict['convergence_group']['charge_neutrality']) < 10**-4
+
