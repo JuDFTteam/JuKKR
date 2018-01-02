afb898cb3390528043a8e576d46bec21dc22c60e
Philipp Rüssmann <p.ruessmann@fz-juelich.de>
2018-01-02 14:25:16 +0100

    Added numpy dependecy

--- source/KKRhost/tests/tools/test_verify_results.py
+++ source/KKRhost/tests/tools/test_verify_results.py
@@ -4,11 +4,13 @@ import pytest
 
 def test_verify_results():
     import os
-    from kkrparser_functions import *
+    from kkrparser_functions import parse_kkr_outputfile
     from pprint import pprint
    
-    path0 = 'test_run/'
+    path0 = './test_run/'
     print_all = False
+
+    print 'running tests for', path0
    
     success, parser_msgs, out_dict = parse_kkr_outputfile({}, path0+'out_kkr', path0+'output.0.txt', path0+'output.000.txt', path0+'out_timing.000.txt', path0+'out_potential', path0+'nonco_angle_out.dat')
    
@@ -23,4 +25,3 @@ def test_verify_results():
     assert success
     assert out_dict['convergence_group']['rms'] < 10**-6
     assert abs(out_dict['convergence_group']['charge_neutrality']) < 10**-4
-
