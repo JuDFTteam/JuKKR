04c17e98ff8ad91e7ac57ccd54903050ef8b2211
Philipp Rüssmann <p.ruessmann@fz-juelich.de>
2018-01-03 11:22:24 +0100

    Restructured and modified test cases

--- source/KKRhost/tests/tools/test_verify_results.py
+++ source/KKRhost/tests/tools/test_verify_results.py
@@ -1,28 +1,39 @@
 #!/usr/bin/env python
 
 import pytest
+import os, pprint
+from kkrparser_functions import parse_kkr_outputfile
 
-def test_verify_results():
-    import os
-    from kkrparser_functions import parse_kkr_outputfile
-    from pprint import pprint
-   
-    path0 = './test_run/'
-    print_all = False
+class Test_check_test_runs():
+    """
+    check results of the tests
+    """
 
-    print 'running tests for', path0
-    print os.listdir(path0)
-   
-    success, parser_msgs, out_dict = parse_kkr_outputfile({}, path0+'out_kkr', path0+'output.0.txt', path0+'output.000.txt', path0+'out_timing.000.txt', path0+'out_potential', path0+'nonco_angle_out.dat')
-   
-    if not success or print_all:
-        if not success:
-            print 'WARNING parsing retured with error(s)'
-            pprint(parser_msgs)
-            print 
-        print 'parsed out_dict:'
-        pprint(out_dict)
-   
-    assert success
-    assert out_dict['convergence_group']['rms'] < 10**-6
-    assert abs(out_dict['convergence_group']['charge_neutrality']) < 10**-4
+    def test_verify1_Au_bulk(self):
+        path0 = './test_run1/'
+        success, parser_msgs, out_dict = parse_kkr_outputfile({}, path0+'out_kkr', path0+'output.0.txt', path0+'output.000.txt', path0+'out_timing.000.txt', path0+'out_potential', path0+'nonco_angle_out.dat')
+	pprint.pprint(parser_msgs)
+	pprint.pprint(out_dict)
+        assert success
+        assert out_dict['convergence_group']['rms'] < 10**-8
+        assert abs(out_dict['convergence_group']['charge_neutrality']) <= 10**-6
+
+
+    def test_verify2_Fe_slab(self):
+        path0 = './test_run2/'
+        success, parser_msgs, out_dict = parse_kkr_outputfile({}, path0+'out_kkr', path0+'output.0.txt', path0+'output.000.txt', path0+'out_timing.000.txt', path0+'out_potential', path0+'nonco_angle_out.dat')
+	pprint.pprint(parser_msgs)
+	pprint.pprint(out_dict)
+        assert success
+        assert out_dict['convergence_group']['rms'] < 10**-8
+        assert abs(out_dict['convergence_group']['charge_neutrality']) <= 10**-6
+
+
+    def test_verify3_Si_lloyd(self):
+        path0 = './test_run3/'
+        success, parser_msgs, out_dict = parse_kkr_outputfile({}, path0+'out_kkr', path0+'output.0.txt', path0+'output.000.txt', path0+'out_timing.000.txt', path0+'out_potential', path0+'nonco_angle_out.dat')
+	pprint.pprint(parser_msgs)
+	pprint.pprint(out_dict)
+        assert success
+        assert out_dict['convergence_group']['rms'] < 10**-8
+        assert abs(out_dict['convergence_group']['charge_neutrality']) <= 10**-6
