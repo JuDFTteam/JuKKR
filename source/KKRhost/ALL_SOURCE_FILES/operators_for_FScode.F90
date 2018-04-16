0f93e6279efff1e2554947475e6d2164740d5775
ruess <p.ruessmann@fz-juelich.de>
2018-04-16 11:41:12 +0200

    now everything that is needed for imp-operator calculation should be there

--- source/KKRhost/ALL_SOURCE_FILES/operators_for_FScode.F90
+++ source/KKRhost/ALL_SOURCE_FILES/operators_for_FScode.F90
@@ -43,6 +43,10 @@ subroutine operators_for_FScode(KORBIT)
   ! constants
   double complex, parameter :: czero=(0.0d0, 0.0d0)
 
+  ! for impurity-wavefunction related stuff
+  double complex, allocatable :: PNS_SO_IMP(:,:,:,:,:) 
+  integer :: i1_imp, natomimp
+
 # ifdef CPP_MPI
   ! communcate PNS_SO_ALL for OPERATOR option
   integer :: ihelp
@@ -203,16 +207,13 @@ subroutine operators_for_FScode(KORBIT)
 
   ! done with preparations, call normcoeff routines that construct operators
   if(myrank==master) WRITE(*,*) 'Computing spin operator'
-  CALL NORMCOEFF_SO(t_params%IRCUT,t_params%LMMAXD/2,PNS_SO_ALL,t_params%THETAS,t_params%NTCELL,t_params%IFUNM,t_params%IPAN,t_params%LMSP,t_inc%KVREL,t_params%CLEB,t_params%ICLEB,t_params%IEND,t_params%DRDI,t_params%IRWS,1+KORBIT)
+  CALL NORMCOEFF_SO(NATYP, t_params%IRCUT,t_params%LMMAXD/2,PNS_SO_ALL,t_params%THETAS,t_params%NTCELL,t_params%IFUNM,t_params%IPAN,t_params%LMSP,t_inc%KVREL,t_params%CLEB,t_params%ICLEB,t_params%IEND,t_params%DRDI,t_params%IRWS,1+KORBIT, 0)
      
   if(myrank==master) WRITE(*,*) 'Computing torq operator'
-  CALL NORMCOEFF_SO_TORQ(t_params%IRCUT,t_params%LMMAXD/2,PNS_SO_ALL,t_params%NTCELL,t_params%IFUNM,t_params%IPAN,t_params%LMSP,t_inc%KVREL,t_params%CLEB,t_params%ICLEB,t_params%IEND,t_params%DRDI,t_params%IRWS,t_params%VISP,t_inc%NSPIN,t_params%VINS,t_params%IRMIN)
+  CALL NORMCOEFF_SO_TORQ(NATYP, t_params%IRCUT,t_params%LMMAXD/2,PNS_SO_ALL,t_params%NTCELL,t_params%IFUNM,t_params%IPAN,t_params%LMSP,t_inc%KVREL,t_params%CLEB,t_params%ICLEB,t_params%IEND,t_params%DRDI,t_params%IRWS,t_params%VISP,t_inc%NSPIN,t_params%VINS,t_params%IRMIN,0)
 
   if(myrank==master) WRITE(*,*) 'Computing spinflux operator'
-  CALL NORMCOEFF_SO_SPINFLUX(t_params%IRCUT,t_params%LMMAXD/2,PNS_SO_ALL,t_inc%KVREL,t_params%DRDI)
-
-  ! deallocate temporary arrays
-  deallocate(irws, rmesh, npan_tot, rpan_intervall, ipan_intervall, theta, phi)
+  CALL NORMCOEFF_SO_SPINFLUX(NATYP, t_params%IRCUT,t_params%LMMAXD/2,PNS_SO_ALL,t_inc%KVREL,t_params%DRDI,0)
 
 
   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -221,17 +222,118 @@ subroutine operators_for_FScode(KORBIT)
 
   ! interpolate impurity wavefunctions to old radial mesh and global spin frame
 
+  NATOMIMP = t_imp%natomimp
+
+  ALLOCATE(PNS_SO_IMP(LMMAXD,LMMAXD,IRMD,2,NATOMIMP))
+  PNS_SO_IMP = CZERO
+
+# ifdef CPP_MPI
+  call distribute_linear_on_tasks(t_mpi_c_grid%nranks_ie, t_mpi_c_grid%myrank_ie+t_mpi_c_grid%myrank_at,master,natomimp,ntot_pT,ioff_pT,.true.)
+  i1_start = ioff_pT(t_mpi_c_grid%myrank_ie) + 1
+  i1_end   = ioff_pT(t_mpi_c_grid%myrank_ie) + ntot_pT(t_mpi_c_grid%myrank_ie)
+  t_mpi_c_grid%ntot1  = ntot_pT(t_mpi_c_grid%myrank_ie)
+  t_mpi_c_grid%ntot_pT1 = ntot_pT
+  t_mpi_c_grid%ioff_pT1 = ioff_pT
+# else
+  i1_start = 1
+  i1_end   = NATOMIMP
+# endif
+
+  DO I1_imp=i1_start, i1_end
+
+    ! use I1 to point to host atom corresponding to position in imp. cluster
+    ! (used to map to correct mesh)
+    i1 = t_params%ATOMIMP(i1_imp)
+    write(*,*) myrank, i1_imp, i1, t_imp%THETAIMP(I1_imp), t_imp%PHIIMP(I1_imp)
+
+    ALLOCATE(RLL(NSRA*LMMAXD,LMMAXD,t_inc%IRMDNEW,0:0))
+    ALLOCATE(PNS_SO(LMMAXD,LMMAXD,IRMD,2))
+    RLL = CZERO
+    PNS_SO = CZERO
+
+    ! read wavefunctions ...
+    RLL(:,:,:,0) = t_imp%RLLIMP(:,:,:,i1_imp)
+
+    ! transform radial wavefunction back to old mesh
+    ALLOCATE(RLLTEMP(t_inc%IRMDNEW,LMMAXD))
+    ALLOCATE(PNSTEMP(IRWS(I1),LMMAXD))
+    DO LM1=1,LMMAXD
+     RLLTEMP=CZERO
+     PNSTEMP=CZERO
+     IR = 0
+     DO IR=1,t_inc%IRMDNEW
+      DO LM2=1,LMMAXD
+       RLLTEMP(IR,LM2)=RLL(LM1,LM2,IR,0)
+      ENDDO
+     ENDDO
+     CALL CHEB2OLDGRID(IRWS(I1),t_inc%IRMDNEW,LMMAXD,RMESH(:,I1), NCHEB, NPAN_TOT(I1),RPAN_INTERVALL(:,I1), IPAN_INTERVALL(:,I1),RLLTEMP,PNSTEMP,IRMD)
+     DO IR=1,IRWS(I1)
+      DO LM2=1,LMMAXD
+       PNS_SO(LM1,LM2,IR,1)=PNSTEMP(IR,LM2)
+      ENDDO
+     ENDDO
+    ENDDO ! LM1
+    ! for small component
+    IF (NSRA.EQ.2) THEN
+     DO LM1=1,LMMAXD
+      RLLTEMP=CZERO
+      PNSTEMP=CZERO
+      DO IR=1,t_inc%IRMDNEW
+       DO LM2=1,LMMAXD
+        RLLTEMP(IR,LM2)=RLL(LM1+LMMAXD,LM2,IR,0)
+       ENDDO
+      ENDDO
+      CALL CHEB2OLDGRID(IRWS(I1),t_inc%IRMDNEW,LMMAXD,RMESH(:,I1), NCHEB, NPAN_TOT(I1),RPAN_INTERVALL(:,I1), IPAN_INTERVALL(:,I1),RLLTEMP,PNSTEMP,IRMD)
+      DO IR=1,IRWS(I1)
+       DO LM2=1,LMMAXD
+        PNS_SO(LM1,LM2,IR,2)=PNSTEMP(IR,LM2)
+       ENDDO
+      ENDDO
+     ENDDO ! LM1
+    ENDIF ! NSRA.EQ.2
+
+    ! rotate radial wavefunction to global frame
+    DO IR=1,IRMD
+      CALL ROTATEMATRIX(PNS_SO(1,1,IR,1), t_imp%THETAIMP(I1_imp), t_imp%PHIIMP(I1_imp), LMMAXD/2,0)
+      CALL ROTATEMATRIX(PNS_SO(1,1,IR,2), t_imp%THETAIMP(I1_imp), t_imp%PHIIMP(I1_imp), LMMAXD/2,0)
+    ENDDO
+
+    ! finally collect wavefuncitons in global frame and old mesh for all atoms to be used in normcoeff-routines below
+    PNS_SO_IMP(:,:,:,:,i1_imp) = PNS_SO(:,:,:,:)
+
+    DEALLOCATE(RLL,PNS_SO)
+    DEALLOCATE(RLLTEMP)
+    DEALLOCATE(PNSTEMP)
+
+  END DO ! I1_imp=i1_start, i1_end
+
+  ! deallocate temporary arrays
+  deallocate(irws, rmesh, npan_tot, rpan_intervall, ipan_intervall, theta, phi)
+
+
+# ifdef CPP_MPI
+  ! finally gather PNS_SO_ALL on master in case of MPI run
+  allocate(work(LMMAXD,LMMAXD,IRMD,2,NATOMIMP), stat=ierr)
+  if(ierr.ne.0) stop 'Error allocating work for MPI comm of PNS_SO_ALL in main1a'
+  ihelp = LMMAXD*LMMAXD*IRMD*2*NATOMIMP
+  call MPI_ALLREDUCE(PNS_SO_IMP, work, ihelp, MPI_DOUBLE_COMPLEX, MPI_SUM, t_mpi_c_grid%myMPI_comm_ie,ierr)
+
+  if(ierr.ne.MPI_SUCCESS) stop 'Error in MPI comm of PNS_SO_ALL in main1a'
+  PNS_SO_IMP(:,:,:,:,:) = work(:,:,:,:,:)
+  deallocate(work, stat=ierr)
+  if(ierr.ne.0) stop 'Error deallocating work for MPI comm of PNS_SO_ALL in main1a'
+# endif
   ! ... => PNS_SO_ALL_IMP
 
   ! construct impurity operators using impurity wavefunctions
-  if(myrank==master) WRITE(*,*) 'Computing impurity spin operator'
-  CALL NORMCOEFF_SO_imp(t_imp%IRCUTIMP,t_params%LMMAXD/2,PNS_SO_ALL_IMP,t_imp%THETASIMP,t_imp%NTCELLIMP,t_imp%IFUNMIMP,t_imp%IPANIMP,t_imp%LMSPIMP,t_inc%KVREL,t_params%CLEB,t_params%ICLEB,t_params%IEND,t_imp%DRDIIMP,t_imp%IRWSIMP,1+KORBIT)
+  !if(myrank==master) WRITE(*,*) 'Computing impurity spin operator'
+  !CALL NORMCOEFF_SO_imp(NATOMIMP, t_imp%IRCUTIMP,t_params%LMMAXD/2,PNS_SO_IMP,t_imp%THETASIMP,t_imp%NTCELLIMP,t_imp%IFUNMIMP,t_imp%IPANIMP,t_imp%LMSPIMP,t_inc%KVREL,t_params%CLEB,t_params%ICLEB,t_params%IEND,t_imp%DRDIIMP,t_imp%IRWSIMP,1+KORBIT)
      
-  if(myrank==master) WRITE(*,*) 'Computing impurity torq operator'
-  CALL NORMCOEFF_SO_TORQ_imp(t_imp%IRCUTIMP,t_params%LMMAXD/2,PNS_SO_ALL_IMP,t_imp%NTCELLIMP,t_imp%IFUNMIMP,t_imp%IPANIMP,t_imp%LMSPIMP,t_inc%KVREL,t_params%CLEB,t_params%ICLEB,t_params%IEND,t_imp%DRDIIMP,t_imp%IRWSIMP,t_imp%VISPIMP,t_inc%NSPIN,t_imp%VINSIMP,t_imp%IRMINIMP)
+  !if(myrank==master) WRITE(*,*) 'Computing impurity torq operator'
+  !CALL NORMCOEFF_SO_TORQ_imp(NATOMIMP, t_imp%IRCUTIMP,t_params%LMMAXD/2,PNS_SO_IMP,t_imp%NTCELLIMP,t_imp%IFUNMIMP,t_imp%IPANIMP,t_imp%LMSPIMP,t_inc%KVREL,t_params%CLEB,t_params%ICLEB,t_params%IEND,t_imp%DRDIIMP,t_imp%IRWSIMP,t_imp%VISPIMP,t_inc%NSPIN,t_imp%VINSIMP,t_imp%IRMINIMP)
 
-  if(myrank==master) WRITE(*,*) 'Computing impurity spinflux operator'
-  CALL NORMCOEFF_SO_SPINFLUX_imp(t_imp%IRCUTIMP,t_params%LMMAXD/2,PNS_SO_ALL_IMP,t_inc%KVREL,t_imp%DRDIIMP)
+  !if(myrank==master) WRITE(*,*) 'Computing impurity spinflux operator'
+  !CALL NORMCOEFF_SO_SPINFLUX_imp(NATOMIMP, t_imp%IRCUTIMP,t_params%LMMAXD/2,PNS_SO_IMP,t_inc%KVREL,t_imp%DRDIIMP)
   
 !check!!!!!!!!!!!!!!!!!!!!
 
