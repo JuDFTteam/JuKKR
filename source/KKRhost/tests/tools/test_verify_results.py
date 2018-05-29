fffe4f53b3c2ca177fbcd747aa3f667a7e104091
Philipp Rüssmann <p.ruessmann@fz-juelich.de>
2018-05-29 14:47:58 +0200

    Updated kkrparser functions used in verify step

--- source/KKRhost/tests/tools/test_verify_results.py
+++ source/KKRhost/tests/tools/test_verify_results.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-import pytest
+#use print('message') instead of print 'message' in python 2.7 as well:
+from __future__ import print_function
+
+#import pytest
 import os, pprint
 from kkrparser_functions import parse_kkr_outputfile
 from numpy import mean, std, array, loadtxt
@@ -147,11 +150,11 @@ class Test_check_test_runs():
            fname = f
            num, text = read_file(path+fname)
            num_ref, text_ref = read_file(path0+fname.split('/')[1])
-           print fname
-           print std(abs(num-num_ref))
-           print mean(abs(num-num_ref))
-           print abs(num-num_ref).max()
-           print set(text)-set(text_ref)==set()
+           print(fname)
+           print(std(abs(num-num_ref)))
+           print(mean(abs(num-num_ref)))
+           print(abs(num-num_ref).max())
+           print(set(text)-set(text_ref)==set())
            assert std(abs(num-num_ref))<5*10**-11
            assert mean(abs(num-num_ref))<10**-12
            assert abs(num-num_ref).max()<2*10**-8
@@ -168,11 +171,11 @@ class Test_check_test_runs():
            text = text[1:]
            text_ref = text_ref[1:]
            # now compare
-           print fname
-           print std(abs(num-num_ref))
-           print mean(abs(num-num_ref))
-           print abs(num-num_ref).max()
-           print set(text)-set(text_ref)==set()
+           print(fname)
+           print(std(abs(num-num_ref)))
+           print(mean(abs(num-num_ref)))
+           print(abs(num-num_ref).max())
+           print(set(text)-set(text_ref)==set())
            assert std(abs(num-num_ref))<5*10**-16
            assert mean(abs(num-num_ref))<10**-14
            assert abs(num-num_ref).max()<2*10**-12
@@ -206,17 +209,17 @@ def cmp_modes(cmplist, path00):
     cmp_values = {'rms':[], 'charges':[]}
     for addpath in cmplist:
         path0 = path00+addpath
-        print path0, os.listdir('.')
+        print(path0, os.listdir('.'))
         if path0 in os.listdir('.'):
             path0+='/'
             success, parser_msgs, out_dict = parse_kkr_outputfile({}, path0+'out_kkr', path0+'output.0.txt', path0+'output.000.txt', path0+'out_timing.000.txt', path0+'out_potential', path0+'nonco_angle_out.dat')
-            print path0
+            print(path0)
             pprint.pprint(parser_msgs)
             #pprint.pprint(out_dict)
             cmp_values['rms'].append(out_dict['convergence_group']['rms'])
             cmp_values['charges'].append(out_dict['charge_valence_states_per_atom'])
         else:
-            print path0, 'not found'
+            print(path0, 'not found')
             success = False
         assert success
     pprint.pprint(cmp_values)
@@ -246,3 +249,8 @@ def read_file(path):
                text.append(i)
    return array(numbers), text
 
+"""
+if __name__=='__main__':
+    path0 = '/Users/ruess/Downloads/tests/test_run1_serial_1_1/'
+    standard_verify(path0, rms_threshold=5*10**-8)
+#"""
