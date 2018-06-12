c7838372cbf49c406baf6fea1017be80d429e4ba
ruess <p.ruessmann@fz-juelich.de>
2018-06-12 11:36:51 +0200

    Added mod_DataTypes to repo

---
+++ source/KKRhost/ALL_SOURCE_FILES/mod_DataTypes.f90
@@ -0,0 +1,15 @@
+module mod_datatypes
+  use, intrinsic :: iso_fortran_env
+
+  implicit none
+  ! everything is private unless otherwise stated
+  private
+  public :: sp, dp, si, di
+
+  integer, parameter :: sp = real32
+  integer, parameter :: dp = real64
+  integer, parameter :: si = int32
+  integer, parameter :: di = int64
+
+
+end module mod_datatypes
