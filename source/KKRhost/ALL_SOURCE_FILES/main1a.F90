3cf0abb5b4a142c91862c4e5b1f39721f29d3be0
ruess <p.ruessmann@fz-juelich.de>
2018-08-16 11:51:50 +0200

    Now data can be passed to BdG test code

--- source/KKRhost/ALL_SOURCE_FILES/main1a.F90
+++ source/KKRhost/ALL_SOURCE_FILES/main1a.F90
@@ -253,47 +253,47 @@ contains
              if (nranks>1) stop 'test option BdG_dev can only be used in serial!'
              if (i1==i1_start) open(887766, file='BdG_tmat_inputs.txt', form='formatted')
              if (i1==1) then 
-               write(887766, '(A)') 'global parameters:'
+               write(887766, '(A25)') 'global parameters:'
                write(887766, *) 
-               write(887766, '(A,I9)') 'IELAST= ', IELAST
-               write(887766, '(A,I9)') 'NSPIN= ', NSPIN
-               write(887766, '(A,I9)') 'LMAX= ', LMAX
-               write(887766, '(A,I9)') 'NSRA= ', NSRA
-               write(887766, '(A,I9)') 'IEND= ', IEND
-               write(887766, '(A,I9)') 'LMPOTD= ', LMPOTD
-               write(887766, '(A,I9)') 'LLY= ', LLY
-               write(887766, '(A,2ES21.9)') 'DELTAE= ', DELTAE
-               write(887766, '(A,I9)') 'IDOLDAU= ', IDOLDAU
-               write(887766, '(A,I9)') 'NCLEB= ', NCLEB
-               write(887766, '(A,I9)') 'NCHEB= ', NCHEB
-               write(887766, '(A,I9)') 'NTOTD= ', NTOTD
-               write(887766, '(A,I9)') 'MMAXD= ', MMAXD
-               write(887766, '(A,I9)') 'NSPIND= ', NSPIND
-               write(887766, '(A,I9)') 'IEMXD= ', IEMXD
-               write(887766, '(A,I9)') 'NRMAXD= ', NRMAXD
-               write(887766, '(A,I9)') 'NSPOTD= ', NSPOTD
-               write(887766, '(A,999999999ES21.9)') 'CLEB= ', CLEB(:,1)
-               write(887766, '(A,999999999I9)') 'ICLEB= ', ICLEB
-               write(887766, '(A,999999999ES21.9)') 'EZ= ', EZ
+               write(887766, '(A25,I9)') 'IELAST= ', IELAST
+               write(887766, '(A25,I9)') 'NSPIN= ', NSPIN
+               write(887766, '(A25,I9)') 'LMAX= ', LMAX
+               write(887766, '(A25,I9)') 'NSRA= ', NSRA
+               write(887766, '(A25,I9)') 'IEND= ', IEND
+               write(887766, '(A25,I9)') 'LMPOTD= ', LMPOTD
+               write(887766, '(A25,I9)') 'LLY= ', LLY
+               write(887766, '(A25,2ES21.9)') 'DELTAE= ', DELTAE
+               write(887766, '(A25,I9)') 'IDOLDAU= ', IDOLDAU
+               write(887766, '(A25,I9)') 'NCLEB= ', NCLEB
+               write(887766, '(A25,I9)') 'NCHEB= ', NCHEB
+               write(887766, '(A25,I9)') 'NTOTD= ', NTOTD
+               write(887766, '(A25,I9)') 'MMAXD= ', MMAXD
+               write(887766, '(A25,I9)') 'NSPIND= ', NSPIND
+               write(887766, '(A25,I9)') 'IEMXD= ', IEMXD
+               write(887766, '(A25,I9)') 'NRMAXD= ', NRMAXD
+               write(887766, '(A25,I9)') 'NSPOTD= ', NSPOTD
+               write(887766, '(A25)') 'CLEB= '
+               write(887766, '(999999999ES21.9)') CLEB(:,1)
+               write(887766, '(A25)') 'ICLEB= '
+               write(887766, '(999999999I9)') ICLEB(:,:)
+               write(887766, '(A25,999999999ES21.9)') 'EZ= ', EZ
                write(887766, *) 
-               write(887766, '(A)') 'atom-dependent input:'
+               write(887766, '(A25)') 'atom-dependent input:'
                write(887766, *) 
              end if 
-             write(887766, '(A,I9)') 'I1= ', I1
-             write(887766, '(A,I9)') 'IPOT= ', IPOT
-             write(887766, '(A,99999999I9)') 'IPOT= ', IPOT
-             write(887766, '(A,I9)') 'NPAN_TOT= ', NPAN_TOT(I1)
-             write(887766, '(A,I9)') 'LPOT= ', LOPT(I1)
-             write(887766, '(A,999999999I9)') 'IPAN_INTERVALL= ', IPAN_INTERVALL(:,I1)
-             write(887766, '(A,ES21.9)') 'ZAT= ', ZAT(I1)
-             write(887766, '(A,ES21.9)') 'PHI= ', PHI(I1)
-             write(887766, '(A,ES21.9)') 'THETA= ', THETA(I1)
-             write(887766, '(A,ES21.9)') 'SOCSCALE= ', SOCSCALE(I1)
-             write(887766, '(A,999999999ES21.9)') 'RNEW= ', RNEW(:,I1)
-             write(887766, '(A,999999999ES21.9)') 'RPAN_INTERVALL= ', RPAN_INTERVALL(:,I1)
-             write(887766, '(A,999999999ES21.9)') 'WLDAU= ', WLDAU(:,:,:,I1)
-             write(887766, '(A,999999999ES21.9)') 'VINSNEW= ', VINSNEW
-             write(887766, '(A,I9)') 'end atom=', I1
+             write(887766, '(A25,I9)') 'I1= ', I1
+             write(887766, '(A25,I9)') 'IPOT= ', IPOT
+             write(887766, '(A25,I9)') 'NPAN_TOT= ', NPAN_TOT(I1)
+             write(887766, '(A25,I9)') 'LPOT= ', LOPT(I1)
+             write(887766, '(A25,999999999I9)') 'IPAN_INTERVALL= ', IPAN_INTERVALL(:,I1)
+             write(887766, '(A25,ES21.9)') 'ZAT= ', ZAT(I1)
+             write(887766, '(A25,ES21.9)') 'PHI= ', PHI(I1)
+             write(887766, '(A25,ES21.9)') 'THETA= ', THETA(I1)
+             write(887766, '(A25,ES21.9)') 'SOCSCALE= ', SOCSCALE(I1)
+             write(887766, '(A25,999999999ES21.9)') 'RNEW= ', RNEW(:,I1)
+             write(887766, '(A25,999999999ES21.9)') 'RPAN_INTERVALL= ', RPAN_INTERVALL(:,I1)
+             write(887766, '(A25,999999999ES21.9)') 'WLDAU= ', WLDAU(:,:,:,I1)
+             write(887766, '(A25,999999999ES21.9)') 'VINSNEW= ', VINSNEW
              write(887766, *)
              if (i1==i1_end) then
                close(887766)
--- source/KKRhost/ALL_SOURCE_FILES/main1a_dummy_BdG.f90
+++ source/KKRhost/ALL_SOURCE_FILES/main1a_dummy_BdG.f90
@@ -32,62 +32,69 @@ subroutine main1a_dummy
   real (kind=dp), dimension(NATYPD) :: THETA
   real (kind=dp), dimension(:,:,:), allocatable :: VINSNEW
 
-  integer :: i1_start, i1_end, ierr,i_stat,i_all
+  integer :: i1_run, i1_start, i1_end, ierr,i_stat,i_all
 
-  do I1=i1_start,i1_end
+  character(len=25) dummy
 
-    IPOT=NSPIN*(I1-1)+1
+  allocate(VINSNEW(NRMAXD,LMPOTD,NSPOTD),stat=i_stat)
+  call memocc(i_stat,product(shape(VINSNEW))*kind(VINSNEW),'VINSNEW','main1a')
+  VINSNEW=0.0D0
+
+  i1_start = 1
+  i1_end = NATYP
+
+  do I1_run=i1_start,i1_end
 
     if (TEST('BdG_dev ')) then
-      ! write out inputs for tmat_newsolver to extract first BdG 
+      ! read out inputs for tmat_newsolver to extract first BdG 
       if (nranks>1) stop 'test option BdG_dev can only be used in serial!'
-      if (i1==i1_start) open(887766, file='BdG_tmat_inputs.txt', form='formatted')
-      if (i1==1) then 
-        write(887766, '(A)') 'global parameters:'
-        write(887766, *) 
-        write(887766, '(A,I9)') 'IELAST= ', IELAST
-        write(887766, '(A,I9)') 'NSPIN= ', NSPIN
-        write(887766, '(A,I9)') 'LMAX= ', LMAX
-        write(887766, '(A,I9)') 'NSRA= ', NSRA
-        write(887766, '(A,I9)') 'IEND= ', IEND
-        write(887766, '(A,I9)') 'LMPOTD= ', LMPOTD
-        write(887766, '(A,I9)') 'LLY= ', LLY
-        write(887766, '(A,2ES21.9)') 'DELTAE= ', DELTAE
-        write(887766, '(A,I9)') 'IDOLDAU= ', IDOLDAU
-        write(887766, '(A,I9)') 'NCLEB= ', NCLEB
-        write(887766, '(A,I9)') 'NCHEB= ', NCHEB
-        write(887766, '(A,I9)') 'NTOTD= ', NTOTD
-        write(887766, '(A,I9)') 'MMAXD= ', MMAXD
-        write(887766, '(A,I9)') 'NSPIND= ', NSPIND
-        write(887766, '(A,I9)') 'IEMXD= ', IEMXD
-        write(887766, '(A,I9)') 'NRMAXD= ', NRMAXD
-        write(887766, '(A,I9)') 'NSPOTD= ', NSPOTD
-        write(887766, '(A,999999999ES21.9)') 'CLEB= ', CLEB(:,1)
-        write(887766, '(A,999999999I9)') 'ICLEB= ', ICLEB
-        write(887766, '(A,999999999ES21.9)') 'EZ= ', EZ
-        write(887766, *) 
-        write(887766, '(A)') 'atom-dependent input:'
-        write(887766, *) 
+      if (i1_run==1) open(887766, file='BdG_tmat_inputs.txt', form='formatted')
+      if (i1_run==1) then 
+        read(887766, *) dummy
+        read(887766, *)  
+        read(887766, *) dummy, IELAST
+        read(887766, *) dummy, NSPIN
+        read(887766, *) dummy, LMAX
+        read(887766, *) dummy, NSRA
+        read(887766, *) dummy, IEND
+        read(887766, *) dummy, LMPOTD
+        read(887766, *) dummy, LLY
+        read(887766, *) dummy, DELTAE
+        read(887766, *) dummy, IDOLDAU
+        read(887766, *) dummy, NCLEB
+        read(887766, *) dummy, NCHEB
+        read(887766, *) dummy, NTOTD
+        read(887766, *) dummy, MMAXD
+        read(887766, *) dummy, NSPIND
+        read(887766, *) dummy, IEMXD
+        read(887766, *) dummy, NRMAXD
+        read(887766, *) dummy, NSPOTD
+        read(887766, *) dummy
+        read(887766, *) CLEB(:,1)
+        read(887766, *) dummy
+        read(887766, *) ICLEB(:,:)
+        read(887766, *) dummy, EZ
+        read(887766, *)
+        read(887766, *) dummy
+        read(887766, *)
       end if 
-      write(887766, '(A,I9)') 'I1= ', I1
-      write(887766, '(A,I9)') 'IPOT= ', IPOT
-      write(887766, '(A,99999999I9)') 'IPOT= ', IPOT
-      write(887766, '(A,I9)') 'NPAN_TOT= ', NPAN_TOT(I1)
-      write(887766, '(A,I9)') 'LPOT= ', LOPT(I1)
-      write(887766, '(A,999999999I9)') 'IPAN_INTERVALL= ', IPAN_INTERVALL(:,I1)
-      write(887766, '(A,ES21.9)') 'ZAT= ', ZAT(I1)
-      write(887766, '(A,ES21.9)') 'PHI= ', PHI(I1)
-      write(887766, '(A,ES21.9)') 'THETA= ', THETA(I1)
-      write(887766, '(A,ES21.9)') 'SOCSCALE= ', SOCSCALE(I1)
-      write(887766, '(A,999999999ES21.9)') 'RNEW= ', RNEW(:,I1)
-      write(887766, '(A,999999999ES21.9)') 'RPAN_INTERVALL= ', RPAN_INTERVALL(:,I1)
-      write(887766, '(A,999999999ES21.9)') 'WLDAU= ', WLDAU(:,:,:,I1)
-      write(887766, '(A,999999999ES21.9)') 'VINSNEW= ', VINSNEW
-      write(887766, '(A,I9)') 'end atom=', I1
-      write(887766, *)
+      read(887766, *) dummy, I1
+      read(887766, *) dummy, IPOT
+      read(887766, *) dummy, NPAN_TOT(I1)
+      read(887766, *) dummy, LOPT(I1)
+      read(887766, *) dummy, IPAN_INTERVALL(:,I1)
+      read(887766, *) dummy, ZAT(I1)
+      read(887766, *) dummy, PHI(I1)
+      read(887766, *) dummy, THETA(I1)
+      read(887766, *) dummy, SOCSCALE(I1)
+      read(887766, *) dummy, RNEW(:,I1)
+      read(887766, *) dummy, RPAN_INTERVALL(:,I1)
+      read(887766, *) dummy, WLDAU(:,:,:,I1)
+      read(887766, *) dummy, VINSNEW
+      read(887766, *) 
       if (i1==i1_end) then
         close(887766)
-        stop 'done writing tmat_newsolver input of test option BdG_dev'
+        stop 'done reading tmat_newsolver input of test option BdG_dev'
       end if
     end if
 
