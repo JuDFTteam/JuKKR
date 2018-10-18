cbd350d2bb012b59aa1e0f250d4ad97627e601a4
Philipp Rüssmann <p.ruessmann@fz-juelich.de>
2018-10-18 20:39:05 +0200

    Corrected kkr.x path for godfrin test

--- source/KKRhost/tests/tools/run_test_20.sh
+++ source/KKRhost/tests/tools/run_test_20.sh
@@ -3,7 +3,7 @@ cd tests
 cp -r test_inputs/test_20_godfrin/ test_run20_hybrid_1_3
 cd test_run20_hybrid_1_3/godfrinON/
 export OMP_NUM_THREADS=1
-mpirun -np 3 ../../kkr.x | tee out_kkr
+mpirun -np 3 ../../../kkr.x | tee out_kkr
 cd ../godfrinOFF/
-mpirun -np 3 ../../kkr.x | tee out_kkr
+mpirun -np 3 ../../../kkr.x | tee out_kkr
 cd ../../../
