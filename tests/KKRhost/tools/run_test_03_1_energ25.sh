375180e9b23e1633e44c3bfba414d64b93adfaad
ruess <p.ruessmann@fz-juelich.de>
2019-03-08 13:02:09 +0100

    Small fix test running script run_test_03_1_energ25.sh

--- tests/KKRhost/tools/run_test_03_1_energ25.sh
+++ tests/KKRhost/tools/run_test_03_1_energ25.sh
@@ -4,5 +4,5 @@ cp -rL test_inputs/test_03.1/ test_run03.1_energ_hybrid_1_25
 cd test_run03.1_energ_hybrid_1_25
 mv inputcard_mpienerg inputcard
 # run either with srun for slurm or directly with mpirun
-srun -N 4 -n 25 ../../kkr.x > out_kkr || mpirun -np 25 ../../kkr.x > tee out_kkr
+srun -N 4 -n 25 ../../kkr.x > out_kkr || mpirun -np 25 ../../kkr.x > out_kkr
 cd ../
