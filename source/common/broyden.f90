e63e1378a680f5089fbc4084f7e0da2e37edc176
ruess <p.ruessmann@fz-juelich.de>
2020-10-20 11:00:06 +0200

    Some more cleanup of Broyden routine

--- source/common/broyden.f90
+++ source/common/broyden.f90
@@ -14,7 +14,8 @@ contains
   !> Deprecated: False ! This needs to be set to True for deprecated subroutines
   !>
   !-------------------------------------------------------------------------------
-  subroutine broyden (vector, vlen, alpha, rms, iter, n_init, mbroylen, mvlen)
+
+  subroutine broyden(vector, vlen, alpha, rms, iter, n_init, broylen, mvlen)
     !
     !cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
     !     History: Original code written by D.D. Johnson (see PRB 38, 12807)
@@ -40,11 +41,10 @@ contains
     !     alpha       -> linear mixing factor
     !     rms         -> RMS difference between old and new vector
     !     iter        -> iteration number (if 1, linear mixing, broyden reset)
-    !     broylen     -> number of iterations that are used for mixing (<=mbroylen)
+    !     broylen     -> number of iterations that are used for mixing
     !     u, vt, f, df, vold, and w are working arrays that need to be saved
     !                   between call to this subroutine
     !     a, b, d, cm, and ipiv are working arrays that need not be saved
-    !     mbroylen    -> maximum number of iterations that can be saved
     !     mvlen       -> maximum length of vectors
     !
     !     See declaration for exact dimentsions and types
@@ -64,183 +64,192 @@ contains
     !
     !cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
     
-    implicit none 
-    !
-    integer :: mbroylen, mvlen ! used for dimensioning 
-    real (kind=dp) :: vector(mvlen,2)
-    integer :: vlen
-    real (kind=dp) :: alpha
-    real (kind=dp) :: rms
-    integer :: iter
-    integer :: n_init,broylen
+    implicit none
+    
+    ! constants
+    real (kind=dp), parameter :: zero=0.0_dp, one=1.0_dp
+
+    ! interface
+    integer, intent(in) :: broylen !! broylen -> number of iterations that are used for mixing
+    integer, intent(in) :: mvlen !! mvlen -> maximum length of vectors
+
+    integer, intent(in) :: vlen !! vlen -> length of vector
+    real (kind=dp), intent(in) :: alpha !! alpha -> linear mixing factor
+    integer, intent(in) :: iter !! iter -> iteration number (if 1, linear mixing, broyden reset)
+    integer, intent(in) :: n_init !! number of simple mixing iterations before broyden mixing starts (>=1)
+    ! are updated in each iteration:
+    real (kind=dp), intent(inout) :: vector(mvlen,2) !! vector(r,i) -> i=1: old vector (input), scratch (ouput)
+                                                     !!             -> i=2: new vector (input), mixed vector (output)
+    real (kind=dp), intent(inout) :: rms !! rms -> RMS difference between old and new vector
+
+    ! local arrays
     real (kind=dp), allocatable, save:: u(:,:)
     real (kind=dp), allocatable, save:: vt(:,:)
     real (kind=dp), allocatable, save:: f(:)
     real (kind=dp), allocatable, save:: df(:)
     real (kind=dp), allocatable, save:: vold(:)
-    real (kind=dp) :: a(mbroylen,mbroylen)
-    real (kind=dp) :: b(mbroylen,mbroylen)
-    real (kind=dp) :: d(mbroylen,mbroylen)
-    real (kind=dp) :: cm(mbroylen)
     real (kind=dp), allocatable, save:: w(:)
-    integer :: ipiv(mbroylen) 
-    !
+    real (kind=dp) :: a(broylen,broylen)
+    real (kind=dp) :: b(broylen,broylen)
+    real (kind=dp) :: d(broylen,broylen)
+    real (kind=dp) :: cm(broylen)
+    integer :: ipiv(broylen)
+
+    ! local scalars
     integer :: i,j,k,info,ntasks
-    real (kind=dp) :: fac1,fac2,fnorm,dfnorm,w0,work,aij,gmi,cmj,wtmp 
-    !
     integer :: lastit,lastm1,nn
-    real (kind=dp), parameter :: zero=0.d0, one=1.d0
+    real (kind=dp) :: fac1,fac2,fnorm,dfnorm,w0,work,aij,gmi,cmj,wtmp 
     real (kind=dp) :: amix
-    save :: lastit,amix 
-    
-    broylen=mbroylen
-    
+
+    save :: lastit, amix 
+        
     if (.not. allocated(u)) then
-    allocate(u(mvlen,mbroylen))
-    allocate(vt(mvlen,mbroylen))
-    allocate(f(mvlen))
-    allocate(df(mvlen))
-    allocate(vold(mvlen))
-    allocate(w(mbroylen))
+      allocate(u(mvlen,broylen))
+      allocate(vt(mvlen,broylen))
+      allocate(f(mvlen))
+      allocate(df(mvlen))
+      allocate(vold(mvlen))
+      allocate(w(broylen))
     end if
     
-    !
     if (iter<=n_init) then
-    !cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
-    !     first n_init iteration: preform linear mixing, load f and vold, set
-    !                      different pointers and variables
-    !cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
-    lastit = iter-1          ! initialize pointers
-    lastm1 = lastit-1 
-    
-    amix = alpha           ! for safety reasons
-    
-    do k = 1,vlen
+      !cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+      !     first n_init iteration: preform linear mixing, load f and vold, set
+      !                      different pointers and variables
+      !cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
+      lastit = iter-1          ! initialize pointers
+      lastm1 = lastit-1 
+      
+      amix = alpha           ! for safety reasons
+      
+      do k = 1,vlen
         f(k) = vector(k,2) - vector(k,1)
         vold(k) = vector(k,1)
-    enddo 
-    
-    do k = 1,vlen          ! this is the linear mixing
+      enddo 
+      
+      do k = 1,vlen          ! this is the linear mixing
         vector(k,2) = vector(k,1) + amix * f(k)
-    enddo 
+      enddo 
     
     else ! (iter<=n_init)
     
-    !cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
-    !     iter > n_init: this is where the non-linear mixing is done
-    !cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
-    lastit = lastit+1      ! update pointers
-    lastm1 = lastit-1 
-    
-    if( iter > broylen) then ! set current lenght of broyden cycle
+      !cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+      !     iter > n_init: this is where the non-linear mixing is done
+      !cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
+      lastit = lastit+1      ! update pointers
+      lastm1 = lastit-1 
+      
+      if( iter > broylen) then ! set current lenght of broyden cycle
         nn = broylen
-    else
+      else
         nn = lastit         !lastm1
-    endif 
-    
-    w0=.01d0               ! set weighting factor for the zeroth iteration 
-    
-    !---- find: f[i] := vector(2)[i] - vector(1)[i]
-    do k = 1,vlen
+      endif 
+      
+      w0=.01d0               ! set weighting factor for the zeroth iteration 
+      
+      !---- find: f[i] := vector(2)[i] - vector(1)[i]
+      do k = 1,vlen
         df(k) = vector(k,2) - vector(k,1) - f(k)
-    enddo
-  
-    do k = 1,vlen
+      enddo
+      
+      do k = 1,vlen
         f(k) = vector(k,2) - vector(k,1)
-    enddo 
-    
-    !---- find: fnorm  := |f|
-    dfnorm = zero
-    fnorm = zero
-    
-    do k = 1,vlen
+      enddo 
+      
+      !---- find: fnorm  := |f|
+      dfnorm = zero
+      fnorm = zero
+      
+      do k = 1,vlen
         dfnorm = dfnorm + df(k)*df(k)
         fnorm  = fnorm  + f(k)*f(k)
-    enddo 
-    
-    dfnorm = sqrt( dfnorm )
-    fnorm  = sqrt( fnorm ) 
-    
-    !---- set: vector(2) := alpha*df/|df| + (vector(1) - vold)/|df|
-    fac2 = one/dfnorm
-    fac1 = amix*fac2
-    
-    do k = 1,vlen
+      enddo 
+      
+      dfnorm = sqrt( dfnorm )
+      fnorm  = sqrt( fnorm ) 
+      
+      !---- set: vector(2) := alpha*df/|df| + (vector(1) - vold)/|df|
+      fac2 = one/dfnorm
+      fac1 = amix*fac2
+      
+      do k = 1,vlen
         vector(k,2) = fac1*df(k) + fac2*(vector(k,1) - vold(k))
         vold(k) = vector(k,1)
         vector(k,1) = fac2*df(k)
-    enddo 
-    
-    !---- store vector(1) and vector(2) in the stacks u and vt restpectively
-    call broy_sav(u,vt,vector,iter-1,broylen,vlen,mvlen) 
-    
-    !---- calculate coefficient matrices, a(i,j), and sum cm(i) for corrections:
-    do j=1,nn - 1          ! off diagonal elements of a(i,j)
+      enddo 
+      
+      !---- store vector(1) and vector(2) in the stacks u and vt restpectively
+      call broy_sav(u,vt,vector,iter-1,broylen,vlen,mvlen) 
+      
+      !---- calculate coefficient matrices, a(i,j), and sum cm(i) for corrections:
+      do j=1,nn - 1          ! off diagonal elements of a(i,j)
         do i = j+1,nn
-        aij = zero
-        do k = 1,vlen
+          aij = zero
+          do k = 1,vlen
             aij = aij + vt(k,j)*vt(k,i)
-        enddo 
-        a(i,j) = aij
-        a(j,i) = aij
+          enddo 
+          a(i,j) = aij
+          a(j,i) = aij
         enddo
-    enddo 
-  
-    do i = 1,nn             ! diagonal elements a(i,i) and cm(i)
+      enddo 
+      
+      do i = 1,nn             ! diagonal elements a(i,i) and cm(i)
         aij = zero
         cmj = zero
         do k=1,vlen
-        cmj = cmj + vt(k,i)*f(k)
-        aij = aij + vt(k,i)*vt(k,i)
+          cmj = cmj + vt(k,i)*f(k)
+          aij = aij + vt(k,i)*vt(k,i)
         enddo 
         a(i,i) = aij
         cm(i) = cmj
-    enddo 
-    
-    !---- shift down weights in stack
-    if(iter-1 > broylen)then
+      enddo 
+      
+      !---- shift down weights in stack
+      if(iter-1 > broylen)then
         do i=1,broylen-1
-        w(i)=w(i+1)
+          w(i)=w(i+1)
         enddo
-    endif
-    
-    wtmp = zero
-    if( rms > 1.0d-09 ) wtmp=2.0*sqrt(0.010d+00/rms)
-    if( wtmp < one )    wtmp=1.00d+00
-    
-    if(iter > broylen)then
+      endif
+      
+      wtmp = zero
+      if( rms > 1.0d-09 ) wtmp=2.0*sqrt(0.010d+00/rms)
+      if( wtmp < one )    wtmp=1.00d+00
+      
+      if(iter > broylen)then
         w(broylen)=wtmp
-    else
+      else
         w(lastit)=wtmp       !w(lastm1)=wtmp
-    endif 
-    
-    !---- now calculate the b-matrix:
-    !>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> uses lapack
-    do i=1,nn
+      endif 
+      
+      !---- now calculate the b-matrix:
+      !>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> uses lapack
+      do i=1,nn
         do j=1,nn
-        b(j,i)= a(j,i)*w(j)*w(i)
+          b(j,i)= a(j,i)*w(j)*w(i)
         enddo
         b(i,i)= w0**2 + a(i,i)*w(i)*w(i)
-    enddo
-    
-    call dgetrf(nn,nn,b,mbroylen,ipiv,info)
-    call dgetri(nn,b,mbroylen, ipiv, d, nn, info )
-    !<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< uses lapack
-  
-    !---- mix vectors: 
-    do k=1,vlen
+      enddo
+      
+      call dgetrf(nn,nn,b,broylen,ipiv,info)
+      call dgetri(nn,b,broylen, ipiv, d, nn, info )
+      !<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< uses lapack
+      
+      !---- mix vectors: 
+      do k=1,vlen
         vector(k,2)= vold(k) + amix * f(k)
-    enddo
-    do i=1,nn
+      enddo
+      do i=1,nn
         gmi = zero
         do j=1,nn
-        gmi = gmi + cm(j)*b(j,i)*w(j)
+          gmi = gmi + cm(j)*b(j,i)*w(j)
         enddo
-    
+      
         do k=1,vlen
-        vector(k,2) = vector(k,2) - gmi*u(k,i)*w(i)
+          vector(k,2) = vector(k,2) - gmi*u(k,i)*w(i)
         enddo
-    enddo 
+      enddo 
     
     endif  ! (iter<=n_init)
   
