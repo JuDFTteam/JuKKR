9a95725ed5aef823ea0143d030ba751800cbd91b
Rudolf Zeller <ru.zeller@fz-juelich.de>
2021-05-25 17:56:43 +0200

    Adds in input possible specification of the a_log parameter of the exponential mesh. Removes hard-coded a_log.

--- source/KKRnano/source/datastructures/InputParams_mod.F90
+++ source/KKRnano/source/datastructures/InputParams_mod.F90
@@ -76,6 +76,7 @@ module InputParams_mod
     integer :: npan_eq
     integer :: ncheb
     double precision :: r_log
+    double precision :: a_log
     logical :: enable_quad_prec
   endtype ! InputParams
 
@@ -616,6 +617,15 @@ integer function getValues(filename, self) result(ierror)
     ierror = 0 ! ok, no error
   elseif (ierror /= 0) then
     write(*,*) "Bad/no value given for r_log."
+    destroy_and_return
+  endif
+
+  ierror = getValue(cr, "a_log", self%a_log , def=0.025D0)
+  if (ierror == use_default) then
+    write(*,*) "WARNING: Bad/no value given for a_log. Set a_log to 0.025D0"
+    ierror = 0 ! ok, no error
+  elseif (ierror /= 0) then
+    write(*,*) "Bad/no value given for a_log."
     destroy_and_return
   endif
 
